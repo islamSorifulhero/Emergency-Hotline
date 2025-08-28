55c3604e33870f241f57056a523a14fb8bbdc559
islamsorifulhero <islamsoriful.hero@gmail.com>
2025-08-28 21:39:49 +0600

    all projects done

--- copy.js
+++ copy.js
@@ -6,13 +6,14 @@ for (const copy of copyBtn) {
         const card = copy.closest(".card");
         const number = card.querySelector(".title-num").innerText;
 
-        alert('number has copied:' + ' ' + number);
+        alert('✅ number has copied:' + ' ' + number);
 
         const copyCount = parseInt(document.getElementById('copy-count').innerText);
 
         const totalCopyCount = copyCount + 1;
         
-                
+        navigator.clipboard.writeText(number);
+        
         document.getElementById('copy-count').innerText = totalCopyCount;
 
     })
